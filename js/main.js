1c8f43c808364de8a3107b2218b366c4d22da842
MaherAhmed0 <maher7ahmed985@gmail.com>
2023-11-06 02:01:14 +0200

    Translation section

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+function copyTextToClipboard() {
+    document.getElementById("textToCopy").select();
+    document.execCommand('copy');
+    window.alert("Text has been copied to the clipboard.");
+    new bootstrap.Tooltip(document.getElementById("textToCopy"), {
+        title: "Copied",
+        placement: "right"
+    }).show();
+}
+
+
